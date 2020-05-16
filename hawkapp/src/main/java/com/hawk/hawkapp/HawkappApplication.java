67a221b5db214742a9d9e219cf8854b72f2cdc8b
shreq <shrek.kresh@o2.pl>
2020-05-16 19:11:02 +0200

    project init

---
+++ hawkapp/src/main/java/com/hawk/hawkapp/HawkappApplication.java
@@ -0,0 +1,12 @@
+package com.hawk.hawkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawkappApplication.class, args);
+	}
+}
---
+++ hawkapp/src/test/java/com/hawk/hawkapp/HawkappApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hawk.hawkapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawkappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
