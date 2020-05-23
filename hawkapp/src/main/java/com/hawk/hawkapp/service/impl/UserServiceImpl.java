c33d5042cbbadcb5dfdaf6f40b5bb9cca126c78d
znowack <z.nowacki@o2.pl>
2020-05-23 19:03:35 +0200

    added age restriction

--- hawkapp/src/main/java/com/hawk/hawkapp/service/impl/UserServiceImpl.java
+++ hawkapp/src/main/java/com/hawk/hawkapp/service/impl/UserServiceImpl.java
@@ -3,8 +3,15 @@ package com.hawk.hawkapp.service.impl;
 import com.hawk.hawkapp.model.User;
 import com.hawk.hawkapp.repository.UserRepository;
 import com.hawk.hawkapp.service.intf.UserService;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 
 @Service
 public class UserServiceImpl extends BaseServiceImpl<UserRepository, User>
@@ -13,5 +20,17 @@ public class UserServiceImpl extends BaseServiceImpl<UserRepository, User>
     @Autowired
     public UserServiceImpl(UserRepository userRepository) {
         super(userRepository);
+    }
+
+    @Override
+    public User add(User user) {
+        LocalDate birthDate = user.getBirthDate().toLocalDateTime().toLocalDate();
+        LocalDate now = new Timestamp(System.currentTimeMillis()).toLocalDateTime().toLocalDate();
+
+        if (Period.between(birthDate, now).getYears() < 16) {
+            throw new IllegalArgumentException();
+        }
+
+        return super.add(user);
     }
 }
