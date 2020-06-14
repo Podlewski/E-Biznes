39f466110ee04916974c30de3edc50045feb9daa
PatrycjaSzczakowska <patrycja.szczakowska@gmail.com>
2020-06-14 16:51:46 +0200

    SearchObjects page

--- hawkapp/web/src/components/SearchObjects.jsx
+++ hawkapp/web/src/components/SearchObjects.jsx
@@ -1,14 +1,66 @@
-import React from "react";
-import { LoggedNavigation } from "./index.js"
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+import { LoggedNavigation } from "./index.js";
+import SingleSearchObject from './SingleSearchObject'
 
-function SearchObjects() {
-  return (
-    <>
-      <LoggedNavigation />
-      <div className="searchObjects">
-      </div>
-    </>
-  );
+
+
+class SearchObjects extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      facilities: [],
+    };
+
+    this.fillData();
+  }
+
+
+  componentDidMount() {
+    this.fillData('http://localhost:8080/facility');
+  }
+
+  fillData(url) {
+    fetch(url,
+      {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      }).then((response) => {
+        if (response.ok) {
+          response.json().then((result) => {
+            this.setState({
+              facilities: result
+            })
+            console.log("state", this.state.facilities);
+          })
+        }
+        else {
+          console.log('Wrong facilities');
+        }
+      })
+  }
+
+  createGridPanel() {
+    let elements = []
+    {
+      this.state.facilities.map(facility => (
+        elements.push(<SingleSearchObject facility={facility} />)
+      ))
+    }
+    return elements
+  }
+
+  render() {
+    return (
+      <>
+        <LoggedNavigation />
+        <div className="searchObjects">
+          {this.createGridPanel()}
+        </div>
+      </>
+    )
+  }
 }
 
-export default SearchObjects;
+export default withRouter(SearchObjects);
