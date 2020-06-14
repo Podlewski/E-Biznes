22e33f0e8f6e26470faf5a9b7a45fa37163270eb
PatrycjaSzczakowska <patrycja.szczakowska@gmail.com>
2020-06-14 06:39:58 +0200

    Authorization with JWT added part2

---
+++ hawkapp/src/main/java/com/hawk/hawkapp/controller/AuthController.java
@@ -0,0 +1,74 @@
+package com.hawk.hawkapp.controller;
+
+import com.hawk.hawkapp.model.User;
+import com.hawk.hawkapp.payload.request.LoginRequest;
+import com.hawk.hawkapp.payload.response.JwtResponse;
+import com.hawk.hawkapp.payload.response.MessageResponse;
+import com.hawk.hawkapp.repository.UserRepository;
+import com.hawk.hawkapp.security.jwt.JwtUtils;
+import com.hawk.hawkapp.security.services.UserDetailsImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt,
+                userDetails.getId(),
+                userDetails.getUsername(),
+                roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody User signUpRequest) {
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        signUpRequest.setPassword(encoder.encode(signUpRequest.getPassword()));
+        userRepository.save(signUpRequest);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+}
