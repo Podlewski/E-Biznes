1b21db0f1603363de5b2bd0b14fdeba98cd1ba09
Karol Podlewski <podlewski@hotmail.com>
2020-06-15 06:47:59 +0200

    [ADD] 404 page

--- hawkapp/web/src/App.js
+++ hawkapp/web/src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Footer, Home, Login, Register, SearchObjects, AccountData } from "./components";
+import "./components/Login.css"
 
 function App() {
+  const NoMatch = () => (
+    <div className="wrapper">
+      <h3>404 - page not found :C</h3>
+    </div>
+  )
+
   return (
     <div className="App">
       <Router>
@@ -13,6 +20,7 @@ function App() {
           <Route path="/register" exact component={() => <Register />} />
           <Route path="/searchObjects" exact component={() => <SearchObjects />} />
           <Route path="/accountData" exact component={() => <AccountData />} />
+          <Route component={NoMatch} />
         </Switch>
         <Footer />
       </Router>
